2a326b57579af86d3ea2ff88e445356c0d66209a
Francisco Javier Jiménez Olivares <francisco.javier.jimenez@ericsson.com>
2019-05-24 20:27:59 +0200

    Foundation inheritance from CardStack removed.

--- src/main/java/klondike/models/Foundation.java
+++ src/main/java/klondike/models/Foundation.java
@@ -1,16 +1,18 @@
 package klondike.models;
 
-public class Foundation extends CardStack {
+public class Foundation {
 
     private Suit suit;
+    private CardStack cardStack;
 
     public Foundation(Suit suit) {
         super();
         this.suit = suit;
+        cardStack = new CardStack(); 
     }
 
     public boolean isComplete() {
-        return this.cards.size() == Number.values().length;
+        return this.cardStack.cards.size() == Number.values().length;
     }
 
     public boolean fitsIn(Card card) {
@@ -23,4 +25,20 @@ public class Foundation extends CardStack {
     public Suit getSuit() {
         return this.suit;
     }
+
+	public void push(Card card) {
+		this.cardStack.push(card);
+	}
+
+	public Card peek() {
+		return this.cardStack.peek();
+	}
+
+	public boolean empty() {
+		return this.cardStack.empty();
+	}
+
+	public Card pop() {
+		return this.cardStack.pop();
+	}
 }
